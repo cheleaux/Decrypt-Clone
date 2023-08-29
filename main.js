e5274fab521b5d812a43c0fb9eb990cb28ae8dbc
Nazareth Dixon <nazareth.dnd@gmail.com>
2023-08-29 22:39:07 +0100

    began implementing header scroll animations

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-window.addEventListener("scroll", () => {
-    const header = document.getElementById ("main-header");
-    header.classList.toggle("sticky", window.scrollY > 0)
-});
+window.addEventListener("scroll", styleStickyHeader)
+
+function styleStickyHeader(){
+    const header = document.getElementById("main-header");
+    const logo = document.getElementById("logo");
+    const logoContainer = document.getElementById("logo");
+
+    ofHeaderScrolled = window.scrollY / 130;
+    header.classList.toggle("sticky", window.scrollY > 0);
+
+    function hideLogo(){
+        if (window.scrollY < 130){                
+            logo.style.width = `max(60px, ${219 * (1 - ofHeaderScrolled)}px)`;
+        }
+
+        if (logo.style.width < 70){
+            logoContainer.style.transform = `translateY(${-(ofHeaderScrolled * 100)}%);`
+        }
+    };
+
+    hideLogo()
+    console.log(-(ofHeaderScrolled * 100))
+};
 

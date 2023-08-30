7d475d1fe39627076d9b8d59313d0e2fbd1c34d4
Nazareth Dixon <nazareth.dnd@gmail.com>
2023-08-30 16:14:32 +0100

    added function the hide header logo onScroll

--- main.js
+++ main.js
@@ -5,20 +5,18 @@ function styleStickyHeader(){
     const logo = document.getElementById("logo");
     const logoContainer = document.getElementById("logo");
 
-    ofHeaderScrolled = window.scrollY / 130;
+    const ofHeaderScrolled = Math.min(1, window.scrollY / 130);
     header.classList.toggle("sticky", window.scrollY > 0);
 
-    function hideLogo(){
-        if (window.scrollY < 130){                
-            logo.style.width = `max(60px, ${219 * (1 - ofHeaderScrolled)}px)`;
-        }
+    (function hideLogo() {
+        const liveLogoWidth = Math.max(60, 219 * (1 - ofHeaderScrolled));
+        const logoScrollEnd = 76;
 
-        if (logo.style.width < 70){
-            logoContainer.style.transform = `translateY(${-(ofHeaderScrolled * 100)}%);`
-        }
-    };
-
-    hideLogo()
-    console.log(-(ofHeaderScrolled * 100))
+        logo.style.width = window.scrollY < 130 ? `${liveLogoWidth}px`:
+            logo.style.width;
+        
+        logoContainer.style.transform = liveLogoWidth < 70 ?
+            `translateY(${Math.min(logoScrollEnd, ofHeaderScrolled * 100)}%)`:
+            logoContainer.style.tramsform;
+    }());
 };
-
